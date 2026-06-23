a1fe7d5b48e9e680bb93b96a13d9a64ea67f8ec3
calvinandersonsasongko-ui <calvinandersonsasongko@gmail.com>
2026-06-23 14:56:52 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,4 +1,4 @@
-import stream as st
+import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
@@ -6,3 +6,19 @@ import os
 
 st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥",
 layout="wide")
+
+@st.cache_resource
+def load_model():
+  with open('hospital_model.pk1')
+   return pickle.load(f)
+
+bundle = load_model()
+model = bundle['model']
+scaler = bundle['scaler']
+features = bundle['features']
+cols_to_scales = bundle['features']
+dept_map_inv = bundle['dept_map_inv']
+temp_map = bundle['temp_map']
+hr_map = bundle['hr_map']
+dur_map = bundle['dur_map']
+cc_map = bundle['cc_map']
