8f0c42a835945caeb524cc14a6fb49659822f470
calvinandersonsasongko-ui <calvinandersonsasongko@gmail.com>
2026-06-23 14:44:27 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,8 @@
+import stream as st
+import pandas as pd
+import numpy as np
+import pickle
+import os
 
+st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥",
+layout="wide)
